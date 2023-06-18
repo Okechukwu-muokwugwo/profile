b71da1555e168da9707a2d99d1878cd6be9584bd
Okechukwu Muokwugwo <okei.muokwugwo@gmail.com>
2023-06-18 08:03:52 +0100

    refactor for desktop view

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,66 +4,63 @@ import {
   FaTwitter, FaLinkedinIn, FaAngellist, FaMedium,
 } from 'react-icons/fa';
 
-const Contact = () => (
-  <div className="contianer mx-auto border-box">
-    <div className="w-full mt-3 mb-3 p-6">
-      <div>
-        <h1 className="mb-2 text-3xl font-bold text-[#4f5461]">Interested in collaborating?</h1>
-        <p className="mt-3 mb-3 text-[#737986]">
-          If you have an application you are interested in developing,
-          a feature that you need built or a project that need coding,
-          I&apos;d love to help you with it.
-        </p>
-      </div>
-    </div>
-    <div
-      className="w-full bg-scroll bg-[url('./img/contact-background.png')] bg-cover bg-no-repeat font-sora"
-    >
-      <form className="flex flex-col p-6">
-        <label htmlFor="name">
-          <input
-            className="w-full mb-4 p-5 border rounded-3xl focus:outline-none"
-            type="text"
-            id="name"
-            placeholder="Full name*"
-            required
-          />
-        </label>
-        <label htmlFor="email">
-          <input
-            className="w-full mb-4 p-5 border rounded-3xl outline-none"
-            type="email"
-            id="email"
-            placeholder="email@gmail.com*"
-          />
-        </label>
+const Contact = () => {
+  const inputClass = 'w-[82vw] md:w-[22.5vw] mt-[18px] md:mb-[21px] p-4 border rounded-3xl focus:outline-none';
+  return (
+    <div className="w-full mx-auto w-full font-roboto">
+      <h1 className="p-6 md:ml-56 md:mx-20 text-3xl text-[#4f5461] font-bold md:pl-20 md:mb-[23px]">Interested in collaborating?</h1>
+      <p className="p-6 md:ml-56 md:mx-20 md:mb-20 md:pl-20 text-[#4f5461] md:w-3/5">
+        If you have an application you are interested in developing,
+        a feature that you need built or a project that need coding,
+        I’d love to help you with it.
+      </p>
+      <form className="w-full bg-[url('./img/contact-background.png')] md:bg-[url('./img/footer-shape.svg')] bg-cover bg-no-repeat">
+        <div className="mx-6 md:ml-56 md:mx-20 flex flex-col md:flex-row md:space-x-10">
+          <label htmlFor="name">
+            <input
+              type="text"
+              id="name"
+              placeholder="Full name*"
+              className={inputClass}
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              type="email"
+              id="email"
+              placeholder="Email*"
+              className={inputClass}
+            />
+          </label>
+        </div>
         <textarea
-          className="w-full mb-4 p-5 border rounded-3xl outline-none placeholder:pl-5 placeholder:pt-5"
-          name="message"
-          id="message"
-          placeholder="Message*"
-          rows="10"
           cols="100"
+          rows="10"
+          placeholder="Message*"
+          className="w-5/6 md:w-1/2 mx-6 mt-[16px] md:ml-56 md:mx-20 md:pl-20 mb-[21px] md:pe-s p-2 border rounded-3xl outline-none placeholder:pl-5 placeholder:pt-5"
         />
-        <button
-          type="submit"
-          className="w-4/5 mx-auto p-4 rounded-3xl bg-[#fa4969] font-bold text-white"
-        >
-          Get in touch
-        </button>
+        <div className="mx-6 md:ml-56">
+          <button
+            type="submit"
+            className="w-full mb-[36px] md:w-1/5 md:ml-10 p-4 rounded-full bg-[#fa4969] font-bold text-white"
+          >
+            Get in touch
+          </button>
+          <div className="flex justify-center md:justify-end items-center md:items-end flex-col md:content-end md:flex-wrap text-[#c5cbfe] md:pr-20">
+            <p className="p-2 text-sm">okei.muokwugwo@gmail.com</p>
+            <div className="w-full flex justify-between md:justify-end p-2 text-xl text-white border-b-2">
+              <AiFillGithub />
+              <FaTwitter />
+              <FaLinkedinIn />
+              <FaAngellist />
+              <FaMedium />
+            </div>
+            <p className="p-2 text-sm">&copy; Okechukwu 2023</p>
+          </div>
+        </div>
       </form>
-      <p className="px-6 mt-3 text-center text-[#c5cbfe]">okei.muokwugwo@gmail.com</p>
-      <div className="flex justify-around space-x-4 p-6 text-3xl text-white">
-        <AiFillGithub />
-        <FaTwitter />
-        <FaLinkedinIn />
-        <FaAngellist />
-        <FaMedium />
-      </div>
-      <hr />
-      <p className="p-6 text-center text-[#c5cbfe] text-lg">&copy; Okechukwu 2023</p>
     </div>
-  </div>
-);
+  );
+};
 
 export default Contact;
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,40 +8,38 @@ import '../App.css';
 const Intro = () => {
   const buttonClass = 'md:w-20 md:p-1 hover:text-center md:text-[#4f5461] md:rounded-full hover:bg-[#e8effa] hover:text-[#4456fb]';
   return (
-    <section className="w-full md:static bg-[url('./img/mobile-background.svg')] bg-cover bg-no-repeat text-white font-sora">
-      <div className="">
-        <div className="p-6">
-          <div className="flex justify-between md:flex-col">
-            <div className="p-2 bg-[#4456fb] rounded-[50%] md:rounded-full md:w-10 md:mb-10 font-bold text-white">OK</div>
-            <nav className="space-x-5 md:flex md:flex-col md:space-x-0 md:space-y-5">
-              <a href="/intro" className={buttonClass}>Intro</a>
-              <a href="/portfolio" className={buttonClass}>Portfolio</a>
-              <a href="/about" className={buttonClass}>About</a>
-              <a href="/contact" className={buttonClass}>Contact</a>
-            </nav>
-          </div>
-          <div className="md:absolute md:left-1/3 md:top-10">
-            <h1 className="mt-5 text-5xl font-bold">Okechukwu</h1>
-            <h2 className="py-2 text-xl font-bold">Software Developer</h2>
-            <p className="mt-4 py-2">
-              Hello I am a software developer! I can help you build a product, featre or website.
-              Look through some of my work and experience! If you like what you see and have
-              project you need coded, don’t hesistate to contact me.
-            </p>
-            <div className="md:relative md:flex md:justify-between md:w-[600px] md:left-2 md:top-2 mt-2 rounded-3xl md:rounded-full bg-white p-4 md:left-10 md:p-1 text-[#4456fb]">
-              <p className="p-2 text-black">My works in social networks</p>
-              <div className="flex justify-between text-2xl p-2 md:space-x-2">
-                <AiFillGithub />
-                <FaTwitter />
-                <FaLinkedinIn />
-                <FaAngellist />
-                <FaMedium />
-              </div>
+    <div className="w-full md:h-[60vw] bg-[url('./img/mobile-background.svg')] md:bg-[url('./img/header-shape.svg')] md:bg-[top_left_250px] bg-cover bg-no-repeat text-white font-sora md:font-roboto">
+      <div className="p-6">
+        <div className="flex justify-between md:flex-col">
+          <div className="p-2 bg-[#4456fb] rounded-full w-10 md:mb-10 font-bold">OK</div>
+          <nav className="space-x-5 md:flex md:flex-col md:space-x-0 md:space-y-5">
+            <a href="/intro" className={buttonClass}>Intro</a>
+            <a href="/portfolio" className={buttonClass}>Portfolio</a>
+            <a href="/about" className={buttonClass}>About</a>
+            <a href="/contact" className={buttonClass}>Contact</a>
+          </nav>
+        </div>
+        <div className="md:absolute md:mx-10 md:left-1/3 md:top-10 ">
+          <h1 className="mt-5 text-5xl font-bold">Okechukwu</h1>
+          <h2 className="text-lg font-bold">Software Developer</h2>
+          <p className="md:w-4/5 mt-4 py-2">
+            Hello I am a software developer! I can help you build a product, feature or website.
+            Look through some of my work and experience! If you like what you see and have
+            project you need coded, don’t hesistate to contact me.
+          </p>
+          <div className="md:relative md:flex md:justify-between md:w-4/5 md:left-2 md:top-2 mt-2 rounded-3xl md:rounded-full bg-white p-4 md:left-10 md:p-1 text-[#4456fb]">
+            <p className="p-2 text-black">My works in social networks</p>
+            <div className="flex justify-between text-2xl p-2 md:space-x-3">
+              <AiFillGithub />
+              <FaTwitter />
+              <FaLinkedinIn />
+              <FaAngellist />
+              <FaMedium />
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
